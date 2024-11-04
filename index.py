7f8f7f661870b46d55fb6b3da66fa01d4ee25a3f
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2024-11-04 14:52:12 -0300

    Commit Initial

---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response
+import json
+
+app = Flask(__name__)
+
+# Carrega o JSON
+with open('BD_Planets.json') as f:
+    data = json.load(f)
+
+@app.route('/')
+def homepage():
+    # Apresenta os dados do JSON na rota raiz da API
+    json_data = json.dumps(data, indent=4)
+    return Response(json_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
